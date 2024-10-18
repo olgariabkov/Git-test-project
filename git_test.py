0e0bf9debaab0790a137d685206cee383189b75f
olga <olga.riabkov@edu.turkuamk.fi>
2024-10-18 11:47:38 +0300

    changes in the development

--- git_test.py
+++ git_test.py
@@ -2,4 +2,4 @@
 # Author:        Olga Riabkov
 # Description:   Code for testing purposes
 
-print("I am a beginner with Git and making changes!") 
+print("I am a beginner with Git and making changes!!") 
